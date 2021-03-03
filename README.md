f44ae067eabc9cddc3363f04dcb0e0ea49b194b6
Natalie-A <aoyanatalie40@gmail.com>
2021-03-03 11:47:33 +0300

    Very first commit

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # lab5
+1.Post data from mpesa to lipa.php
+2.generate an access token
+3.define variables to be used for an stk push
+4.successful stk push
---
+++ lipa.php
@@ -0,0 +1,65 @@
+<?php
+    if(isset($_POST['submit'])){
+    date_default_timezone_set('Africa/Nairobi');
+
+  //generate access token
+  $consumerKey = 'uo1HTtHIOAOXAdtAKAJhmHGQuMXZ04s8';
+  $consumerSecret = 's81E3YDAjFaM74uW';
+
+  // define the variales
+  $BusinessShortCode = '174379';
+  $Passkey = 'bfb279f9aa9bdbcf158e97dd71a467cd2e0c893059b10f78e6b72ada1ed2c919';  
+  $PartyA = $_POST['phoneNumber'];
+  $AccountReference = 'CART056';
+  $TransactionDesc = 'test';
+  $Amount = $_POST['amount'];
+  $Timestamp = date('YmdHis');    
+  $CallBackURL = 'http://localhost.com/api/callback_url.php'; 
+  $Password = base64_encode($BusinessShortCode.$Passkey.$Timestamp);
+
+  $headers = ['Content-Type:application/json; charset=utf8'];
+
+  $access_token_url = 'https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials';
+  $initiate_url = 'https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest';
+
+   
+
+  $curl = curl_init($access_token_url);
+  curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+  curl_setopt($curl, CURLOPT_RETURNTRANSFER, TRUE);
+  curl_setopt($curl, CURLOPT_HEADER, FALSE);
+  curl_setopt($curl, CURLOPT_USERPWD, $consumerKey.':'.$consumerSecret);
+  $result = curl_exec($curl);
+  $status = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+  $result = json_decode($result);
+  $access_token = $result->access_token;  
+  curl_close($curl);
+
+  $stkheader = ['Content-Type:application/json','Authorization:Bearer '.$access_token];
+
+  $curl = curl_init();
+  curl_setopt($curl, CURLOPT_URL, $initiate_url);
+  curl_setopt($curl, CURLOPT_HTTPHEADER, $stkheader); 
+
+  $curl_post_data = array(
+    'BusinessShortCode' => $BusinessShortCode,
+    'Password' => $Password,
+    'Timestamp' => $Timestamp,
+    'TransactionType' => 'CustomerPayBillOnline',
+    'Amount' => $Amount,
+    'PartyA' => $PartyA,
+    'PartyB' => $BusinessShortCode,
+    'PhoneNumber' => $PartyA,
+    'CallBackURL' => $CallBackURL,
+    'AccountReference' => $AccountReference,
+    'TransactionDesc' => $TransactionDesc
+  );
+
+  $data_string = json_encode($curl_post_data);
+  curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+  curl_setopt($curl, CURLOPT_POST, true);
+  curl_setopt($curl, CURLOPT_POSTFIELDS, $data_string);
+  $curl_response = curl_exec($curl);
+  print_r($curl_response);
+    }
+?>
